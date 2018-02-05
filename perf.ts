5de0365bac7993efa39e70b21dbf3ea6c9552ccb
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-02-05 11:26:13 -0800

    Some features for perf.ts

--- perf.ts
+++ perf.ts
@@ -1,9 +1,25 @@
 import sh = require('shelljs')
 import fs = require('fs')
 import argparse = require('argparse')
+
 const parser = new argparse.ArgumentParser()
 parser.addArgument(['-o', '--overwrite'], { action: 'storeTrue' })
-const overwrite = parser.parseArgs()['overwrite'] || false
+parser.addArgument(['--scenario'], { action: 'store' })
+parser.addArgument(['--pass'], { action: 'store' })
+parser.addArgument(['--commit'], { action: 'store' })
+const scenarioMap: { [s: string]: number } = {
+    Angular: 0,
+    Compiler: 1,
+    'Compiler - Unions': 2,
+    Monaco: 3,
+    TFS: 4
+}
+const args = parser.parseArgs()
+const overwrite = args['overwrite'] || false
+
+// const progress = new ProgressBar(process.stderr, { clear: true });
+// const logger = new Logger(LogLevel.Verbose, { out: process.stdout, err: process.stderr, progress });
+// const host = new HostContext(logger);
 function runPerf(commit: string) {
     const filename = commit + '.benchmark'
     if (sh.test('-e', filename) && !overwrite) {
@@ -13,8 +29,9 @@ function runPerf(commit: string) {
     sh.exec('git checkout ' + commit)
     sh.exec('jake clean')
     sh.exec('jake tsc')
-    sh.exec('node internal/scripts/perf/out/ts-perf-cli/cli.js benchmark --iterations 10 --save ~/src/perf2.7/' + filename)
     sh.popd()
+    // await perf.benchmark({ iterations: 10, save: [commit + ".benchmark"], tsc: '~/TypeScript/built/local/tsc.js', suite: "~/TypeScript/internal/cases/perf/solutions" }, host)
+    sh.exec('node ~/TypeScript/internal/scripts/perf/bin/ts-perf benchmark --iterations 10 --save ~/src/perf2.7/' + filename)
 }
 function loadCommits(path: string) {
     const commits = fs.readFileSync(path, 'utf8').split('\n')
@@ -25,4 +42,43 @@ function loadCommits(path: string) {
         runPerf(commit)
     }
 }
-loadCommits('./merge-commits.txt')
+function diffCommits(path: string) {
+    const commits = fs.readFileSync(path, 'utf8').split('\n')
+    const baseline = JSON.parse(fs.readFileSync(commits[0] + '.benchmark', 'utf8')) as Benchmark
+    const scenario = args['scenario'] ? scenarioMap[args['scenario']] : 0
+    const pass = args['pass'] && args['pass'] === 'check' ? 'checkTime' : 'emitTime'
+    for (const commit of commits) {
+        diffPerf(baseline, scenario, pass, commit)
+    }
+    console.log(` --------- ${args['scenario']} (${scenario}): ${pass} ------------`)
+}
+interface Benchmark {
+    measurements: Scenario[]
+}
+interface Scenario {
+    scenarioName: string
+    checkTime: Metric
+    emitTime: Metric
+}
+interface Metric {
+    relativeMarginOfError: number
+    mean: number
+}
+function diffPerf(baseline: Benchmark, scenario: number, pass: 'checkTime' | 'emitTime', commit: string) {
+    const benchmark = JSON.parse(fs.readFileSync(commit + '.benchmark', 'utf8')) as Benchmark
+    const warning = (baseline.measurements[scenario][pass].relativeMarginOfError > 0.05 ? '*' : '') +
+        (benchmark.measurements[scenario][pass].relativeMarginOfError > 0.05 ? '*' : '')
+    const diff = benchmark.measurements[scenario][pass].mean - baseline.measurements[scenario][pass].mean
+    console.log(warning + (diff / baseline.measurements[scenario][pass].mean * 100) + "%")
+    // const b: Benchmark = await perf.Benchmark.loadAsync(baseline + '.benchmark', host)
+    // sh.exec(`node ~/TypeScript/internal/scripts/perf/bin/ts-perf benchmark --load ${commit}.benchmark --baseline ${baseline}.benchmark`)
+}
+if (args['scenario'] || args['pass']) {
+    diffCommits('./merge-commits.txt')
+}
+else if (args['commit']) {
+    runPerf(args['commit'])
+}
+else {
+    loadCommits('./merge-commits.txt')
+}
